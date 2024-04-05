06d0099db561ac911e30afcda1aa93c36b662093
MinaDoncheva <mina.doncheva@icloud.com>
2024-04-05 10:59:52 +0300

    fix path

--- codbex-hestia/api/ProductService.ts
+++ codbex-hestia/api/ProductService.ts
@@ -1,5 +1,5 @@
 import { ProductRepository as ProductDao } from "codbex-products/gen/dao/Products/ProductRepository";
-import { ProductCategoryRepository as CategoryDao } from "codbex-products/gen/dao/Products/ProductCategoryRepository";
+import { ProductCategoryRepository as CategoryDao } from "codbex-products/gen/dao/Categories/ProductCategoryRepository";
 
 import { Controller, Get } from "sdk/http";
 
