8465e09e62e11bbe29d1b292b875573fbc582aff
nedelcho-delchev-tues <nedelcho.delchev@gmail.com>
2024-04-12 16:36:47 +0300

    Added products in the sidebar

---
+++ codbex-hestia/services/categories.js
@@ -0,0 +1,11 @@
+const viewData = {
+    id: "codbex-hestia-categories",
+    label: "Categories",
+    lazyLoad: true,
+    link: "/services/web/codbex-products/gen/ui/Categories/index.html?embedded"
+};
+if (typeof exports !== 'undefined') {
+    exports.getView = function () {
+        return viewData;
+    }
+}
