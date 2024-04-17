27aa8aeb03aa5311d3bbb94fb47c4f351461dd04
nedelcho-delchev-tues <nedelcho.delchev@gmail.com>
2024-04-17 16:11:57 +0300

    fix perspectives

--- codbex-hestia/services/sales-invoices.js
+++ codbex-hestia/services/sales-invoices.js
@@ -2,7 +2,7 @@ const viewData = {
     id: "codbex-hestia-sales-invoices",
     label: "Sales Invoices",
     lazyLoad: true,
-    link: "/services/web/codbex-invoices/gen/ui/SalesInvoice/index.html?embedded"
+    link: "/services/web/codbex-invoices/gen/ui/salesinvoice/index.html?embedded"
 };
 if (typeof exports !== 'undefined') {
     exports.getView = function () {
