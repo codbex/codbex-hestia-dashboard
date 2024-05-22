ade28e79872d67c4dbb529db91cb191135bc983b
MinaDoncheva <mina.doncheva@icloud.com>
2024-05-22 16:27:36 +0300

    fix catalogues perspective name

--- codbex-hestia/services/catalogues.js
+++ codbex-hestia/services/catalogues.js
@@ -1,8 +1,8 @@
 const viewData = {
-    id: "codbex-hestia-catalogue",
-    label: "Catalogue",
+    id: "codbex-hestia-catalogues",
+    label: "Catalogues",
     lazyLoad: true,
-    link: "/services/web/codbex-products/gen/ui/Catalogue/index.html?embedded"
+    link: "/services/web/codbex-products/gen/ui/Catalogues/index.html?embedded"
 };
 if (typeof exports !== 'undefined') {
     exports.getView = function () {
