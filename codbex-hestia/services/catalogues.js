f131a45cb2af11a744790e1f627ab713b005b6df
MinaDoncheva <133380007+MinaDoncheva@users.noreply.github.com>
2024-05-26 23:55:21 +0300

    Add Products Catalogue (#26)
    * add catalogue view
    
    * add catalogue button
    
    * fix catalogues perspective name
    
    * Renamed to single form from plural
    
    ---------
    
    Co-authored-by: nedelcho-delchev-tues <nedelcho.delchev@gmail.com>

---
+++ codbex-hestia/services/catalogues.js
@@ -0,0 +1,11 @@
+const viewData = {
+    id: "codbex-hestia-catalogues",
+    label: "Catalogues",
+    lazyLoad: true,
+    link: "/services/web/codbex-products/gen/ui/Catalogues/index.html?embedded"
+};
+if (typeof exports !== 'undefined') {
+    exports.getView = function () {
+        return viewData;
+    }
+}
