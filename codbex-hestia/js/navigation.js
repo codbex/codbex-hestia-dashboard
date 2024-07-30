92c24ae2bd555dbc4773df6aab603ca2b70afb1f
delchev <delchevn@gmail.com>
2024-07-30 21:20:38 +0300

    sidebar as json

--- codbex-hestia/js/navigation.js
+++ codbex-hestia/js/navigation.js
@@ -1,16 +1,5 @@
-/*
- * Copyright (c) 2010-2024 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
- *
- * SPDX-FileCopyrightText: 2010-2024 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
- * SPDX-License-Identifier: EPL-2.0
- */
 const navigation = angular.module("launchpad", ["ngResource", "ideLayout", "ideUI"]);
-navigation.controller("LaunchpadViewController", ["$scope", "messageHub", function ($scope, messageHub) {
+navigation.controller("LaunchpadViewController", ["$scope", "messageHub", "$http", function ($scope, messageHub, $http) {
     $scope.currentViewId = 'dashboard';
 
     $scope.extraExtensionPoints = ['codbex-orders-dialog-window'];
@@ -20,11 +9,104 @@ navigation.controller("LaunchpadViewController", ["$scope", "messageHub", functi
         $scope.currentViewId = id;
     };
 
-
     messageHub.onDidReceiveMessage('launchpad.switch.perspective', function (msg) {
         $scope.$apply(function () {
             $scope.switchView(msg.data.perspectiveId);
         });
     }, true)
+
+    $scope.sections = [
+        {
+            "name": "Documents", "groups": [
+                {
+                    "name": "Purchasing", "expanded": "purchasingExpanded", "icon": "credit-card",
+                    "items": [
+                        { "name": "Purchase Orders", "view": "purchase-orders" },
+                        { "name": "Purchase Invoices", "view": "purchase-invoices" },
+                        { "name": "Supplier Payments", "view": "supplier-payments" }
+                    ]
+                },
+                {
+                    "name": "Sales", "expanded": "salesExpanded", "icon": "currency",
+                    "items": [
+                        { "name": "Sales Orders", "view": "sales-orders" },
+                        { "name": "Sales Invoices", "view": "sales-invoices" },
+                        { "name": "Customer Payments", "view": "customer-payments" }
+                    ]
+                },
+                {
+                    "name": "Inventory", "expanded": "inventoryExpanded", "icon": "retail-store",
+                    "items": [
+                        { "name": "Goods Receipts", "view": "goods-receipts" },
+                        { "name": "Goods Issues", "view": "goods-issues" },
+                        { "name": "Delivery Note", "view": "delivery-note" },
+                        { "name": "Stock Adjustments", "view": "stock-adjustments" }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Reports", "groups": [
+                {
+                    "name": "Finance", "expanded": "financeReportsExpanded", "icon": "area-chart",
+                    "items": [
+                        { "name": "Sales Orders", "view": "sales-orders-report" },
+                        { "name": "Sales Orders Total", "view": "sales-orders-total-report" },
+                        { "name": "Purchase Orders", "view": "purchase-orders-report" },
+                        { "name": "Purchase Orders Total", "view": "purchase-orders-total-report" }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Products", "groups": [
+                {
+                    "name": "Products", "expanded": "productExpanded", "icon": "product",
+                    "items": [
+                        { "name": "Products", "view": "products" },
+                        { "name": "Categories", "view": "categories" },
+                        { "name": "Catalogue", "view": "catalogues" }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "People", "groups": [
+                {
+                    "name": "Employees", "expanded": "peopleExpanded", "icon": "company-view",
+                    "items": [
+                        { "name": "Organisations", "view": "organisations" },
+                        { "name": "Employees", "view": "employees" }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Settings", "groups": [
+                {
+                    "name": "Partners", "expanded": "partnersExpanded", "icon": "customer-and-contacts",
+                    "items": [
+                        { "name": "Customers", "view": "customers" },
+                        { "name": "Suppliers", "view": "suppliers" },
+                        { "name": "Manufacturers", "view": "manufacturers" }
+                    ]
+                },
+                {
+                    "name": "Configurations", "expanded": "configurationsExpanded", "icon": "wrench",
+                    "items": [
+                        { "name": "Countries", "view": "countries" },
+                        { "name": "Cities", "view": "cities" },
+                        { "name": "Companies", "view": "companies" },
+                        { "name": "Stores", "view": "stores" },
+                        { "name": "Companies", "view": "companies" },
+                        { "name": "Currencies", "view": "currencies" },
+                        { "name": "Dimensions", "view": "dimensions" },
+                        { "name": "Units of Measures", "view": "uoms" },
+                        { "name": "Methods", "view": "methods" }
+                    ]
+                }
+            ]
+        }
+    ];
 
 }]);
