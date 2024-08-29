1f86b90a1f653d4abf639ad74a5e3c110b735805
TIVMOF <tomyivanov06@gmail.com>
2024-08-29 14:57:32 +0000

    feat: finding the path

--- codbex-hestia/api/WidgetsExtension/WidgetService.js
+++ codbex-hestia/api/WidgetsExtension/WidgetService.js
@@ -3,13 +3,23 @@ import { response } from "sdk/http";
 
 const widgetList = [];
 const widgetExtensions = extensions.getExtensions("portal-widgets");
+
+
+
 for (let i = 0; i < widgetExtensions.length; i++) {
     const extensionPath = widgetExtensions[i];
 
+    console.log("test2: " + extensionPath);
+
+    console.log("test2: " + `../../../${extensionPath}`);
+
     const { getWidget } = await import(`../../../${extensionPath}`);
+
+    console.log(typeof getWidget);
+
     const widget = getWidget();
     widgetList.push(widget);
-    debugger
 }
 
-response.println(JSON.stringify(widgetList));
+response.println("hello " + JSON.stringify(widgetList));
+console.log("test");
