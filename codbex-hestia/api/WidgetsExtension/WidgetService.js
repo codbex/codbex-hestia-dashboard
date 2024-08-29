9b74bceb97d757f8145c6f65231fcc76a99b8c8f
TIVMOF <tomyivanov06@gmail.com>
2024-08-29 16:07:04 +0000

    fix: now finding right path

--- codbex-hestia/api/WidgetsExtension/WidgetService.js
+++ codbex-hestia/api/WidgetsExtension/WidgetService.js
@@ -1,19 +1,22 @@
 import { extensions } from "sdk/extensions";
 import { response } from "sdk/http";
 
+const { getWidget } = await import("../../../codbex-orders-widgets/services/average-sales-order-price.js");
+console.log(typeof getWidget);
+
 const widgetList = [];
 const widgetExtensions = extensions.getExtensions("portal-widgets");
-
-
 
 for (let i = 0; i < widgetExtensions.length; i++) {
     const extensionPath = widgetExtensions[i];
 
+    let path = `../../../${extensionPath}`;
+    // let path = "../../../codbex-orders-widgets/services/average-sales-order-price.js";
     console.log("test2: " + extensionPath);
 
-    console.log("test2: " + `../../../${extensionPath}`);
+    console.log("test2: " + path);
 
-    const { getWidget } = await import(`../../../${extensionPath}`);
+    const { getWidget } = await import(path);
 
     console.log(typeof getWidget);
 
